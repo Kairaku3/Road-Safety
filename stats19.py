8375c4f3baf0093aa80b7abb356c709c9d98be9d
Ahmad Rami AlMasrifi <36641006+Kairaku3@users.noreply.github.com>
2022-04-29 14:26:25 +0200

    Add files via upload

--- stats19.py
+++ stats19.py
@@ -15,31 +15,11 @@ def lookup(kind):
     import road safety data guide
     """
     url = "https://data.dft.gov.uk/road-accidents-safety-data/Road-Safety-Open-Dataset-Data-Guide.xlsx"
-    df = pd.read_excel(url, index_col=0, usecols=[0, 1, 2, 3]).loc[kind]
-    df.set_index("field name", drop=True, inplace=True)
-    return df
-## creating code/format:label pair values
-def load_descriptions(group_data):
-    code_label_dict = dict(zip(group_data['code/format'], group_data['label']))
-    return ( 
-        pd.DataFrame({'code': code_label_dict.keys(), 'format': code_label_dict.values()}).dropna()
-    )
-## creating code/label pair values for each field name
-def lookup_pairs(kind):
-    """
-    get pair values for each field name
-    """
     df = (
-    lookup(kind).groupby('field name')
-    .apply(load_descriptions)
-    .reset_index(level=1, drop=True)
-    .reset_index()
-    .rename(columns={'field name': 'field_name'})
-    .groupby('field_name')
-    .apply(lambda group: dict(zip(group['code'], group['format'])))
-    .to_frame()
-    .rename(columns={0:'descriptions'})
+        pd.read_excel(url, index_col=0, usecols=[0, 1, 2, 3])
+        .loc[kind.title()]
+        .dropna()
+        .reset_index(drop=True)
     )
     return df
-
 
